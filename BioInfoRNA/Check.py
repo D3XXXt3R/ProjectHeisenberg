8876ed1ed0d6a13060d8c39a834dd07773cdb4d6
D3XXXt3R <pawelkalecinski94@gmail.com>
2016-06-13 21:04:10 +0200

    File Dialogs, Improving Validation
    File Dialogs, Improving Validation

--- BioInfoRNA/Check.py
+++ BioInfoRNA/Check.py
@@ -2,19 +2,12 @@ import re
 
 
 class Check:
-    def checkLetters(self, seq):
-        i = 0
-        chars = ('A', 'C', 'G', 'U', 'R', 'Y', 'M', 'K', 'W', 'S', 'B', 'D', 'H', 'V', 'N')
-        while i < len(seq):
-            if seq[i].upper() not in chars:
-                return True
-            i += 1
-
     def checkCharacters(self, seq):
         i = 0
-        signs = ('.', '(', ')', '[', ']', '{', '}', '<', '>', '?', '\n')
+        chars = ('A', 'C', 'G', 'U', 'R', 'Y', 'M', 'K', 'W', 'S', 'B', 'D', 'H', 'V', 'N',
+                 '.', '(', ')', '[', ']', '{', '}', '<', '>', '?')
         while i < len(seq):
-            if seq[i] not in signs:
+            if seq[i].upper() not in chars:
                 return True
             i += 1
 
@@ -27,14 +20,8 @@ class Check:
                 or (len(seq) < 6 and not (("(" in seq) or (")" in seq))):
             return True
 
-    def checkResult(self, seq, seq2):
-        counter = 0
-        chars = ('C', 'G', 'U', 'A')
-        numbers = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
-        for i in seq:
-            if (len(seq2) == 6 and not (seq2 == " ")):
-                seq2 += " " + str(i)
-            else:
-                seq2 += str(i)
-            counter += 1
-        print(seq2)
+    def checkSize(self, list1):
+        i = 0
+        while i < (len(list1) - 1):
+            if list1[i] > list1[i-1] or list1[i] < list1[i-1]:
+                return True
--- BioInfoRNA/Main.py
+++ BioInfoRNA/Main.py
@@ -1,38 +1,52 @@
 from selenium import webdriver
-from ProjectHeisenberg.BioInfoRNA.MyHTMLParser import MyHTMLParser
-from ProjectHeisenberg.BioInfoRNA.Check import Check
+from Check import Check
 from bs4 import BeautifulSoup
+from tkinter import filedialog, Tk
 
 
 class Main:
     C = Check()
-    table = []
     sequence = ""
     counter = 0
-    names = ("No.", "PDB id", "NDB id", "Sequence", "Secondary Structure",
-             "Chain", "Start", "End  ", "Method", "Class", "PDB deposition", "Å", "Models")
-    file = open('Text.txt', 'w', encoding='utf-8')
-    browser = webdriver.PhantomJS("C:/Users/Dante/Desktop/phantom/bin/phantomjs")
     # browser = webdriver.Chrome("C:/Users/Dante/Desktop/chromedriver.exe")
+    browser = webdriver.PhantomJS("C:/Users/Dante/Desktop/phantom/bin/phantomjs")
     browser.get("http://rnafrabase.cs.put.poznan.pl")
     button = browser.find_element_by_name("send")
     text1 = browser.find_element_by_id("sequences")
-    text1.clear()
+    names = ("No.", "PDB id", "NDB id", "Sequence", "Secondary Structure",
+             "Chain", "Start", "End  ", "Method", "Class", "PDB deposition", "Å", "Models")
+
+    print("Upload structure from file?")
+    upload = input()
+    if upload.lower() == "yes":
+        root = Tk()
+        root.fileName = filedialog.askopenfilename()
+        root.destroy()
+        fileIn = open(root.fileName, 'r', encoding='utf-8')
     flag = 1
     lines = []
     while True:
+        text1.clear()
         if flag > 1:
             text1.send_keys("\n" + ">strand" + str(flag) + "\n")
         else:
             text1.send_keys(">strand" + str(flag) + "\n")
-        print("Please input sequence(s) and/or secondary structure(s) given in the dot-bracket notation")
-        while True:
-            seq = input()
-            if seq:
-                lines.append(seq)
-            else:
-                break
-        seq = '\n'.join(lines)
+        if upload.lower() == "yes":
+            text1.clear()
+            seq = fileIn.read()
+            fileIn.close()
+            text1.send_keys(seq)
+            button.click()
+            break
+        else:
+            print("Please input sequence(s) and/or secondary structure(s) given in the dot-bracket notation")
+            while True:
+                seq = input()
+                if seq:
+                    lines.append(seq)
+                else:
+                    break
+            seq = '\n'.join(lines)
         if C.checkNumbers(seq) and (C.checkLetters(seq) or C.checkCharacters(seq)):  # C.checkLength(seq)
             print("Sequence is too short or bad input")
             print("Please, try again")
@@ -41,19 +55,28 @@ class Main:
             print("Next sequence?")
             answer = input()
         if answer.lower() == 'no':
-            break
+            url1 = "http://rnafrabase.cs.put.poznan.pl/index.php"
+            button.click()
+            url2 = str(browser.current_url)
+            if url1 == url2:
+                print("Bad input, please restart program")
+                quit()
+            else:
+                break
         elif not (answer.lower() == 'yes'):
             print("Bad answer")
         if answer.lower() == 'yes':
             flag += 1
 
-    button.click()
     tmp = browser.page_source
     soup = BeautifulSoup(browser.page_source, "html.parser")
-
-    parser = MyHTMLParser()
     print("Do you want to save data to a file?")
     answer2 = input()
+    if answer2.lower() == "yes":
+        root = Tk()
+        root.fileName = filedialog.askopenfilename()
+        root.destroy()
+        fileOut = open(root.fileName, 'w', encoding='utf-8')
     for row in (soup.find_all(attrs={"class": ["row_table1", "row_table2"]})):
         for i in row.find_all("td"):
             if counter == 14:
@@ -61,16 +84,19 @@ class Main:
             if counter % 13 == 0 and counter != 0:
                 sequence += " "
                 counter += 1
-                file.write(sequence + "\n")
-                print(sequence)
+                if answer2.lower() == "yes":
+                    fileOut.write(sequence + "\n")
+                else:
+                    print(sequence)
                 sequence = ""
             elif not i.text == "" or (names[counter] == "Å") or sequence == " ":
                 sequence += names[counter] + " " + i.get_text('\n' + names[counter] + " ").strip()
                 counter += 1
                 if answer2.lower() == "yes":
-                    file.write(sequence + "\n")
+                    fileOut.write(sequence + "\n")
                     sequence = ""
                 else:
                     print(sequence)
-                    sequence = ""
-    file.close()
+                sequence = ""
+    if answer2.lower == "yes":
+        fileOut.close()
